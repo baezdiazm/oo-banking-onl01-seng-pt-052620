d744cfebb984e6bbee4eecdfbec261ba8157c186
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-20 07:02:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,71 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def sender
+    @sender
+  end
+
+  def receiver
+    @receiver
+  end
+
+  def status
+    @status
+  end
+
+  def amount
+    @amount
+  end
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def transfer
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    self.status = "complete"
+  end
+
+  def reject
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def valid_trans
+    if @sender.valid? && @sender.balance > @amount && self.status == "pending"
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if self.valid_trans
+      self.transfer
+    else
+      self.reject
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      self.status = "reversed"
+    else
+      self.reject
+    end
+  end
 end
